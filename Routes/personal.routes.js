d24e944a54ed66419d45546b460a4f9daeadb62e
Lakhan9525 <lakhanraj987@gmail.com>
2023-03-12 17:57:52 +0530

    patch

--- Routes/personal.routes.js
+++ Routes/personal.routes.js
@@ -37,18 +37,45 @@ router.get('/:id', async (req, res) => {
 });
 
 // Update a user info
+// router.patch('/:id', async (req, res) => {
+//   const updates = Object.keys(req.body);
+//   const allowedUpdates = ['account_id', 'first_name', 'last_name', 'email', 'cell_phone', 'home_phone', 'photo', 'address'];
+//   const isValidOperation = updates.every(update => allowedUpdates.includes(update));
+//   if (!isValidOperation) {
+//     return res.status(400).send({ error: 'Invalid updates!' });
+//   }
+//   try {
+//     const userInfo = await UserInfo.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+//     if (!userInfo) {
+//       return res.status(404).send();
+//     }
+//     res.send(userInfo);
+//   } catch (error) {
+//     res.status(400).send(error);
+//   }
+// });
+
 router.patch('/:id', async (req, res) => {
-  const updates = Object.keys(req.body);
-  const allowedUpdates = ['account_id', 'first_name', 'last_name', 'email', 'cell_phone', 'home_phone', 'photo', 'address'];
-  const isValidOperation = updates.every(update => allowedUpdates.includes(update));
-  if (!isValidOperation) {
-    return res.status(400).send({ error: 'Invalid updates!' });
-  }
+  const {account_id, first_name, last_name, email, cell_phone, home_phone, photo, address} =req.body
+  const {id}=req.params;
+
   try {
-    const userInfo = await UserInfo.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
-    if (!userInfo) {
-      return res.status(404).send();
-    }
+    const userInfo = await UserInfo.findByIdAndUpdate(
+
+
+      {_id:id},
+      {account_id:account_id,
+         first_name:first_name,
+          last_name:last_name, 
+          email:email, 
+          cell_phone:cell_phone,
+           home_phone:home_phone,
+            photo:photo,
+             address:address},
+             {new:true}
+
+      );
+    
     res.send(userInfo);
   } catch (error) {
     res.status(400).send(error);
