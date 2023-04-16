9c6ea5e2499f79e603576712c8f539a5fdd43bc3
Lakhan9525 <lakhanraj987@gmail.com>
2023-04-16 13:33:22 +0530

    corrct clt

--- Routes/auth.js
+++ Routes/auth.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const passport = require("passport");
 
 //const CLIENT_URL = "http://localhost:3000/";
-const CLT_URL = "https://magical-dolphin-b8849e.netlify.app/"
+const CLT_URL = "https://magical-dolphin-b8849e.netlify.app"
 
 const CLIENT_URL = process.env.CLT_URL || "http://localhost:3000";
 
